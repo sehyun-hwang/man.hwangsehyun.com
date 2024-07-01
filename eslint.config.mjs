6f265570964fc0daf1b53fd6a091731fbe7e2ea0
Sehyun Hwang <hwanghyun3@gmail.com>
2024-07-02 00:36:49 +0900

    Move eslint config to git root

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,40 @@
-import config from './src/eslint.config.js';
+import globals from 'globals';
 
-export default config;
+export default [{
+  files: ['src/*.js'],
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+  rules: {
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+          '_attachments',
+        ],
+      },
+    ],
+  },
+}, {
+  files: ['browser/**/*.js', 'eslint.config.mjs'],
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+  rules: {
+    'no-console': 'error',
+  },
+}, {
+  files: ['assets/js/*.js'],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+    },
+  },
+  rules: {},
+}];
