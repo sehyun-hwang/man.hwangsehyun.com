3af7ca5be2e62f82d1cde0ef71cc21487a6a9ff8
Sehyun Hwang <hwanghyun3@gmail.com>
2024-08-08 17:24:42 +0900

    Chrome binary globing

--- browser/lib/puppeteer-browser.js
+++ browser/lib/puppeteer-browser.js
@@ -1,9 +1,15 @@
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+
 import puppeteer from 'puppeteer-core';
 
-export default () => puppeteer.launch({
-  executablePath: '/usr/local/bin/playwright-browsers/chromium-1117/chrome-linux/chrome',
-  defaultViewport: {
-    width: 1072,
-    height: 1072,
-  },
-});
+const BROWSER_FOLDER = '/usr/local/bin/playwright-browsers'
+
+export default () => readdir(BROWSER_FOLDER)
+  .then(folders => puppeteer.launch({
+    executablePath: join(BROWSER_FOLDER, folders.find(folder => folder.includes('chromium')), 'chrome-linux/chrome'),
+    defaultViewport: {
+      width: 1072,
+      height: 1072,
+    },
+  }));
